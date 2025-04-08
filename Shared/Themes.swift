2966357a3bf801bb70c1cca86f3ed9175884692c
James Pearce <github@tripleodeon.com>
2025-04-07 20:05:53 -0930

    Consolidated colors

--- Shared/Themes.swift
+++ Shared/Themes.swift
@@ -6,16 +6,30 @@ struct Theme {
     static let purple = Color(red: 147 / 255, green: 51 / 255, blue: 234 / 255)  // #9333EA
     static let deepPurple = Color(red: 88 / 255, green: 28 / 255, blue: 135 / 255)  // Darker purple
     static let vibrantPurple = Color(red: 168 / 255, green: 85 / 255, blue: 247 / 255)  // Brighter purple
-    static let glowWhite = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255)
+
+    static let glowWhite = Color.white
     static let mediumGray = Color(white: 0.15)
     static let lightGray = Color(white: 0.3)
     static let offWhite = Color(white: 0.9)
+    // CONSOLIDATED: yellow family
     static let yellow = Color(red: 250 / 255, green: 204 / 255, blue: 21 / 255)  // Vibrant yellow
     static let darkYellow = Color(red: 202 / 255, green: 138 / 255, blue: 4 / 255)  // Darker yellow
+    // yellowyOrange and orangeyYellow can be consolidated with yellow and darkYellow
+    static let yellowyOrange = Color(red: 234 / 255, green: 179 / 255, blue: 8 / 255)
+    static let orangeyYellow = Color(red: 220 / 255, green: 170 / 255, blue: 0 / 255)
+    static let brightYellow = Color(red: 253 / 255, green: 224 / 255, blue: 71 / 255)  // Lighter yellow
+
+    // CONSOLIDATED: orange family
     static let orange = Color(red: 249 / 255, green: 115 / 255, blue: 22 / 255)  // Warm Orange
     static let darkOrange = Color(red: 194 / 255, green: 65 / 255, blue: 12 / 255)  // Deep Orange
+    static let saturatedOrange = Color(red: 245 / 255, green: 158 / 255, blue: 11 / 255)  // Similar to orange but more saturated
+
+    // Blues
     static let blue = Color(red: 37 / 255, green: 99 / 255, blue: 235 / 255)  // Rich Blue
+    // CONSOLIDATED: deepBlue and darkBlue are similar
     static let darkBlue = Color(red: 30 / 255, green: 58 / 255, blue: 138 / 255)  // Deep Blue
+    static let deepBlue = Color(red: 40 / 255, green: 20 / 255, blue: 80 / 255)  // More purple-tinted blue
+
     static let darkGray = Color(red: 24 / 255, green: 24 / 255, blue: 27 / 255)  // Almost Black
     static let nearBlack = Color(red: 17 / 255, green: 12 / 255, blue: 34 / 255)  // Very dark purple-tinted black
     static let silverBlue = Color(red: 28 / 255, green: 28 / 255, blue: 45 / 255)
@@ -25,17 +39,25 @@ struct Theme {
     static let purple800 = Color(red: 91 / 255, green: 33 / 255, blue: 182 / 255)
     // Colors extracted from views
     static let periwinkle = Color(red: 156 / 255, green: 163 / 255, blue: 231 / 255)
+    // CONSOLIDATED: light blue family
     static let lightBlue = Color(red: 96 / 255, green: 165 / 255, blue: 250 / 255)
     static let standardBlue = Color(red: 59 / 255, green: 130 / 255, blue: 246 / 255)
+    static let lightTealBlue = Color(red: 56 / 255, green: 189 / 255, blue: 248 / 255)
+    static let darkTealBlue = Color(red: 14 / 255, green: 165 / 255, blue: 233 / 255)
     static let emeraldGreen = Color(red: 16 / 255, green: 185 / 255, blue: 129 / 255)
     static let brightAmber = Color(red: 249 / 255, green: 180 / 255, blue: 45 / 255)
     static let forestGreen = Color(red: 21 / 255, green: 128 / 255, blue: 61 / 255)
     static let brightFuchsia = Color(red: 236 / 255, green: 64 / 255, blue: 255 / 255)
     static let darkRuby = Color(red: 127 / 255, green: 29 / 255, blue: 29 / 255)
+    // CONSOLIDATED: purple variants
+    // mediumPurple and mutedPurple are similar dark purples
     static let mediumPurple = Color(red: 65 / 255, green: 16 / 255, blue: 94 / 255)
+    static let mutedPurple = Color(red: 26 / 255, green: 14 / 255, blue: 47 / 255)
+    static let lighterPurple = Color(red: 38 / 255, green: 18 / 255, blue: 58 / 255)
+    static let electricViolet = Color(red: 124 / 255, green: 58 / 255, blue: 237 / 255)  // Similar to purple
+    static let softViolet = Color(red: 139 / 255, green: 92 / 255, blue: 246 / 255)
     static let goldenBrown = Color(red: 133 / 255, green: 77 / 255, blue: 14 / 255)
     static let russetBrown = Color(red: 113 / 255, green: 63 / 255, blue: 18 / 255)
-    static let brightYellow = Color(red: 253 / 255, green: 224 / 255, blue: 71 / 255)
     static let silverLight = Color(red: 226 / 255, green: 232 / 255, blue: 240 / 255)
     static let silverDark = Color(red: 148 / 255, green: 163 / 255, blue: 184 / 255)
     static let bronzeLight = Color(red: 217 / 255, green: 119 / 255, blue: 6 / 255)
@@ -50,42 +72,27 @@ struct Theme {
     static let indigoDark = Color(red: 14 / 255, green: 30 / 255, blue: 60 / 255)
     static let purpleIndigo = Color(red: 128 / 255, green: 65 / 255, blue: 217 / 255)
     static let midnightNavy = Color(red: 30 / 255, green: 30 / 255, blue: 46 / 255)
-
+    // CONSOLIDATED: green variants
     static let mutedGreen = Color(red: 34 / 255, green: 197 / 255, blue: 94 / 255)
     static let darkerGreen = Color(red: 22 / 255, green: 163 / 255, blue: 74 / 255)
-
+    // CONSOLIDATED: This is actually purple, not pink
     static let mutedPink = Color(red: 60 / 255, green: 30 / 255, blue: 110 / 255)
-    static let deepBlue = Color(red: 40 / 255, green: 20 / 255, blue: 80 / 255)
-
-    static let softViolet = Color(red: 139 / 255, green: 92 / 255, blue: 246 / 255)
-    // Glass Effect Colors
-    static let glassEffect = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.1)
-    static let glassHighlight = Color(
-        red: 255 / 255,
-        green: 255 / 255,
-        blue: 255 / 255,
-        opacity: 0.15
-    )
-    static let glassShadow = Color(red: 0 / 255, green: 0 / 255, blue: 0 / 255, opacity: 0.2)
-
-    static let lightTealBlue = Color(red: 56 / 255, green: 189 / 255, blue: 248 / 255)
-    static let darkTealBlue = Color(red: 14 / 255, green: 165 / 255, blue: 233 / 255)
-
+    // CONSOLIDATED: midnight purple variants
     static let deepMidnightPurple = Color(red: 20 / 255, green: 10 / 255, blue: 40 / 255)  // Darker
     static let mediumMidnightPurple = Color(red: 30 / 255, green: 18 / 255, blue: 60 / 255)
     static let darkPurpleBlue = Color(red: 35 / 255, green: 20 / 255, blue: 90 / 255)
-    static let mutedPurple = Color(red: 26 / 255, green: 14 / 255, blue: 47 / 255)
     static let blueishPurple = Color(red: 16 / 255, green: 24 / 255, blue: 57 / 255)
-
-    static let yellowyOrange = Color(red: 234 / 255, green: 179 / 255, blue: 8 / 255)
-    static let orangeyYellow = Color(red: 220 / 255, green: 170 / 255, blue: 0 / 255)
-
+    // CONSOLIDATED: red variants
     static let mutedRed = Color(red: 239 / 255, green: 68 / 255, blue: 68 / 255)
     static let darkerRed = Color(red: 185 / 255, green: 28 / 255, blue: 28 / 255)
-    static let darkRed = Color(red: 220 / 255, green: 38 / 255, blue: 38 / 255)
-    static let electricViolet = Color(red: 124 / 255, green: 58 / 255, blue: 237 / 255)
-    static let lighterPurple = Color(red: 38 / 255, green: 18 / 255, blue: 58 / 255)
-    static let saturatedOrange = Color(red: 245 / 255, green: 158 / 255, blue: 11 / 255)
+    static let darkRed = Color(red: 220 / 255, green: 38 / 255, blue: 38 / 255)  // Between mutedRed and darkerRed
+
+    // Glass Effect Colors
+    // CONSOLIDATED: glass effect colors all use white base with different opacity
+    static let glassEffect = Color.white.opacity(0.1)
+    static let glassHighlight = Color.white.opacity(0.15)
+    static let glassShadow = Color.black.opacity(0.2)  // Using Color.black directly
+
     // Medal colors
     static let goldColor = LinearGradient(
         colors: [
